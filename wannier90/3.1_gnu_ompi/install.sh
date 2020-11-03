ebe6140b778df789b54da37fc64b25c9fd829a20
Alexander Hampel <ahampel@flatironinstitute.org>
2020-11-03 09:21:31 -0500

    first commit for w90 ompi version

---
+++ wannier90/3.1_gnu_ompi/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# installation script for wannier90 with GNU OpenMPI toolchain
+
+# load modules
+module purge
+module load gcc/7.4.0 openmpi4/4.0.5 intel/mkl/2019-3 lib/fftw3/3.3.8-openmpi4
+
+# clone version 3.1. from github
+git clone -b v3.1.0 https://github.com/wannier-developers/wannier90.git wannier90
+
+
+cp make.inc wannier90/
+
+cd wannier90
+
+# check modules
+module list
+
+# build wannier90
+make -j 4 wannier lib post
+
+# copy binaries
+mkdir ../bin
+cp postw90.x wannier90.x libwannier.a ../bin/
+
+# change module path
+path=`pwd`
+echo $path
+sed -i "s|REPLACEDIR|$path|g" ../module.file
+
