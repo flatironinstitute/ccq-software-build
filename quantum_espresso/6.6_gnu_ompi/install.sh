9657dd6fd2ffbc10eaf883abc00eb4640cbb46b1
Nicholas Carriero <njcarriero@gmail.com>
2020-11-23 13:55:56 -0500

    Clean up, parameterize module list, add explicit install dir, add logging.

--- quantum_espresso/6.6_gnu_ompi/install.sh
+++ quantum_espresso/6.6_gnu_ompi/install.sh
@@ -3,35 +3,44 @@
 # installation script for wannier90 with GNU OpenMPI toolchain
 
 # load modules
+MODULES="gcc/7.4.0 openmpi4/4.0.5 intel/mkl/2019-3 lib/hdf5/1.8.21-openmpi4" 
 module purge
-module load gcc/7.4.0 openmpi4/4.0.5 intel/mkl/2019-3 lib/hdf5/1.8.21-openmpi4 
+module load ${MODULES}
 
-# clone version 6.6 from github
-git clone -b qe-6.6 https://github.com/QEF/q-e.git qe
+BUILDDIR=$(mktemp -d /dev/shm/qe_build_XXXXXXXX)
+INSTALLDIR="$(pwd)/Installation"
 
-cd qe
+log=build_$(date +%Y%m%d%H%M).log
+testlog="$(pwd)/${log/.log/_test.log}"
+(
+    cd ${BUILDDIR}
 
-./configure -enable-parallel=yes -with-scalapack=yes 
+    # clone version 6.6 from github
+    git clone -b qe-6.6 https://github.com/QEF/q-e.git qe
+    cd qe
+    
+    ./configure -enable-parallel=yes -with-scalapack=yes -prefix=${INSTALLDIR}
 
-# HDF does not work yet as HDF is not compiled with -enable-fortran2003, and -enable-parallel
-#-with-hdf5=$HDF5_BASE
+    # HDF does not work yet as HDF is not compiled with -enable-fortran2003, and -enable-parallel
+    #-with-hdf5=$HDF5_BASE
 
-# build all
-make all
+    # build all
+    make -j 20 all
 
-# run tests
-cd test-suite
-make run-tests-pw-parallel
+    # run tests
+    cd test-suite
+    make -j 20 run-tests-pw-parallel &> ${testlog}
 
-# change module path
-cd ../../
-path=`pwd`
-# clear module files first
-rm module
-# place magic module string
-echo '#%Module' > module
-# add module body
-cat src.module >> module
-# add cwd as root dir
-sed -i "s|REPLACEDIR|$path|g" module
+    # install it
+    cd ..
+    make install 
+) &> ${log}
 
+echo "Last 20 lines of test ouput:"
+tail -20 ${testlog}
+
+# update module template
+sed "s|REPLACEDIR|${INSTALLDIR}|g;s|MODULES|${MODULES}|g" < src.module > module
+
+# finish up
+echo -e "\nReview ${log} and ${testlog}, move the "'"'module'"'" file to the correct location and then run:\n    rm -rf ${BUILDDIR}.\n"
