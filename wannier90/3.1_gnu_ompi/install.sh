2c2a58ffe8bf5d96c43593a8d6c9fb579edb1548
Alexander Hampel <ahampel@flatironinstitute.org>
2020-11-30 17:22:23 -0500

    updated w90 install module files according to latest style agreement

--- wannier90/3.1_gnu_ompi/install.sh
+++ wannier90/3.1_gnu_ompi/install.sh
@@ -3,36 +3,46 @@
 # installation script for wannier90 with GNU OpenMPI toolchain
 
 # load modules
+MODULES="gcc/7.4.0 openmpi4/4.0.5 intel/mkl/2019-3 lib/fftw3/3.3.8-openmpi4" 
 module purge
-module load gcc/7.4.0 openmpi4/4.0.5 intel/mkl/2019-3 lib/fftw3/3.3.8-openmpi4
+module load ${MODULES}
 
-# clone version 3.1. from github
-git clone -b v3.1.0 https://github.com/wannier-developers/wannier90.git wannier90
+BUILDDIR=$(mktemp -d /dev/shm/w90_build_XXXXXXXX)
+INSTALLDIR="$(pwd)"
 
+log=build_$(date +%Y%m%d%H%M).log
+testlog="$(pwd)/${log/.log/_test.log}"
+(
+    cd ${BUILDDIR}
+    module list
 
-cp make.inc wannier90/
+    # clone version 3.1. from github
+    git clone -b v3.1.0 https://github.com/wannier-developers/wannier90.git wannier90
 
-cd wannier90
+    cp ${INSTALLDIR}/make.inc wannier90/
 
-# check modules
-module list
+    cd wannier90
 
-# build wannier90
-make -j 4 wannier lib post
+    # build wannier90
+    make -j 8 wannier lib post w90chk2chk
+    
+    # run tests
+    make tests &> ${testlog}
 
-# copy binaries
-mkdir ../bin
-cp postw90.x wannier90.x libwannier.a ../bin/
+    # copy binaries
+    mkdir ${INSTALLDIR}/bin
+    cp postw90.x wannier90.x libwannier.a w90chk2chk.x ${INSTALLDIR}/bin/
+) &> ${log}
 
-# change module path
-cd ..
-path=`pwd`
-# clear module files first
-rm module
-# place magic module string
+echo "Last 20 lines of test ouput:"
+tail -20 ${testlog}
+
+# make the template a proper module by placing the magic string
 echo '#%Module' > module
-# add module body
-cat src.module >> module
-# add cwd as root dir
-sed -i "s|REPLACEDIR|$path|g" module
+# update module template
+sed "s|REPLACEDIR|${INSTALLDIR}|g;s|MODULES|${MODULES}|g" < src.module >> module
+
+# finish up
+echo -e "\nReview ${log} and ${testlog}, move the "'"'module'"'" file to the correct location and then run:\n    rm -rf ${BUILDDIR}.\n"
+
 
