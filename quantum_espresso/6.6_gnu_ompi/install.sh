4f6d81dbf60cce378df2f2b672c1b016d1014f73
Alexander Hampel <ahampel@flatironinstitute.org>
2020-11-30 16:09:43 -0500

    reviewed latest changes from scicomp
    - reverted back to removing magic line in module file as this otherwise
      gives multiple module in module avail
    - changed the multicore compilation to 10 instead of 20 cores and output
      the loaded module

--- quantum_espresso/6.6_gnu_ompi/install.sh
+++ quantum_espresso/6.6_gnu_ompi/install.sh
@@ -8,12 +8,14 @@ module purge
 module load ${MODULES}
 
 BUILDDIR=$(mktemp -d /dev/shm/qe_build_XXXXXXXX)
-INSTALLDIR="$(pwd)/Installation"
+INSTALLDIR="$(pwd)/install"
 
 log=build_$(date +%Y%m%d%H%M).log
 testlog="$(pwd)/${log/.log/_test.log}"
 (
     cd ${BUILDDIR}
+    
+    module list
 
     # clone version 6.6 from github
     git clone -b qe-6.6 https://github.com/QEF/q-e.git qe
@@ -25,11 +27,11 @@ testlog="$(pwd)/${log/.log/_test.log}"
     #-with-hdf5=$HDF5_BASE
 
     # build all
-    make -j 20 all
+    make -j 10 all
 
     # run tests
     cd test-suite
-    make -j 20 run-tests-pw-parallel &> ${testlog}
+    make run-tests-pw-parallel &> ${testlog}
 
     # install it
     cd ..
@@ -39,8 +41,10 @@ testlog="$(pwd)/${log/.log/_test.log}"
 echo "Last 20 lines of test ouput:"
 tail -20 ${testlog}
 
+# make the template a proper module 
+echo '#%Module' > module
 # update module template
-sed "s|REPLACEDIR|${INSTALLDIR}|g;s|MODULES|${MODULES}|g" < src.module > module
+sed "s|REPLACEDIR|${INSTALLDIR}|g;s|MODULES|${MODULES}|g" < src.module >> module
 
 # finish up
 echo -e "\nReview ${log} and ${testlog}, move the "'"'module'"'" file to the correct location and then run:\n    rm -rf ${BUILDDIR}.\n"
