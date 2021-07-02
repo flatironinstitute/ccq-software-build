1376b3395abdc856e6faf30b0f80e687b459b3eb
Yubo "Paul" Yang <pyang@flatironinstitute.org>
2021-07-02 08:59:59 -0400

    add QMCPACK 3.11.0 GPU version

--- qmcpack/3.11.0_gnu_ompi/gpu_install.sh
+++ qmcpack/3.11.0_gnu_ompi/gpu_install.sh
@@ -4,7 +4,7 @@ NAME="qmcpack"
 VERSION="3.11.0"
 NVER=${NAME}-${VERSION}
 URL="git@github.com:QMCPACK/qmcpack.git"
-MODULES="gcc/7.4.0 openmpi4/4.0.5 intel/mkl/2020-4 lib/hdf5/1.8.21-openmpi4 lib/boost/1.70-gcc7 python3/3.7.3 cmake/3.19.1"
+MODULES="gcc/7.4.0 openmpi4/4.0.5 intel/mkl/2020-4 lib/hdf5/1.8.21-openmpi4 lib/boost/1.70-gcc7 python3/3.7.3 cmake/3.19.1 cuda/10.1.243_418.87.00 nccl/2.4.2-cuda-10.1"
 
 # load modules
 module purge
@@ -29,11 +29,16 @@ testlog="$(pwd)/${log/.log/_test.log}"
 
   export HDF5_ROOT=$HDF5_BASE
   export BOOST_ROOT=$BOOST_BASE
+  export CUDA_HOME=$CUDA_BASE
+  export NCCL_HOME=$NCCL_BASE
 
-  mkdir cpu_comp
-  cd cpu_comp
+  mkdir gpu_comp
+  cd gpu_comp
   cmake -D CMAKE_C_COMPILER=mpicc -D CMAKE_CXX_COMPILER=mpicxx \
    -D BUILD_AFQMC=1 -D QMC_COMPLEX=1 -D QMC_MIXED_PRECISION=1 \
+   -D ENABLE_CUDA=1 -D CUDA_ARCH="sm_70" -D BUILD_AFQMC_WITH_NCCL=1 \
+   -D QMC_INCLUDE="${CUDA_HOME}/include;${NCCL_HOME}/include" \
+   -D QMC_EXTRA_LIBS="-L${CUDA_HOME}/lib64 -Wl,-rpath,${CUDA_HOME} -lcusolver -lcurand -lcusparse -lcublas -lcudart -L${NCCL_HOME}/lib -lnccl -Wl,-rpath=${NCCL_HOME}/lib" \
    -D ENABLE_TIMERS=1 -D BUILD_LMYENGINE_INTERFACE=0 \
    ..
   make -j 16
@@ -44,33 +49,23 @@ testlog="$(pwd)/${log/.log/_test.log}"
 
 tail -20 ${testlog}
 
-# install binaries and scripts
+# install just one binary
 if [ ! -d bin ]; then
   mkdir bin
 fi
-rsync -az ${BUILDDIR}/$NVER/cpu_comp/bin bin/cpu_comp
-rsync -az ${BUILDDIR}/$NVER/nexus .
-rm -rf nexus/tests  # something in here messes with Modules
-rsync -az ${BUILDDIR}/$NVER/utils .
+rsync -az ${BUILDDIR}/$NVER/gpu_comp/bin bin/gpu_comp
 (
   cd bin
-  ln -s cpu_comp/qmcpack qmcpack_cpu_comp
-  ln -s cpu_comp/* .
-  rm qmcpack qmcpack.settings
-  ln -s ../nexus/bin/* .
-  ln -s ../utils/afqmctools/bin/* .
+  ln -s gpu_comp/qmcpack qmcpack_gpu_comp
 )
 
 # make the template a proper module 
-fmod=cpu-rome
-fmod1=cpu-skylake
+fmod=gpu
 echo '#%Module' > $fmod
 # update module template
 sed "s|REPLACEDIR|${INSTALLDIR}|g;s|MODULES|${MODULES}|g" < src.module >> $fmod
 sed -i "s|REPLACE_NAME|${NAME}|" $fmod
 sed -i "s|REPLACE_VERSION|${VERSION}|" $fmod
-# create one for skylake
-sed "s|openmpi4/4.0.5|openmpi4/4.0.5-opa|g" < $fmod > $fmod1
 
 # manual finish up
-echo -e "\nReview ${log} and ${testlog}, move the $fmod $fmod1 files to the correct location and then run:\n    rm -rf ${BUILDDIR}.\n"
+echo -e "\nReview ${log} and ${testlog}, move ${fmod} to the correct location and then run:\n    rm -rf ${BUILDDIR}.\n"
